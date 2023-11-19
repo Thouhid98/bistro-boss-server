f60e536d42f423d8bcb2ac69471bee065fd84e50
Thouhid98 <thouhidhussain98@gmail.com>
2023-11-19 23:55:48 +0600

    Admin update item done

--- index.js
+++ index.js
@@ -162,13 +162,32 @@ async function run() {
     })
 
     // Update Menu Items 
-    // First load the data
+    //Step 01: First load the data
     app.get('/menu/:id', async(req, res)=>{
       const id = req.params.id;
       const query = {_id: new ObjectId(id)}
       const result = await menuCollection.findOne(query);
       res.send(result)
     })
+    
+    // Step 02: Update the data 
+    app.patch('/menu/:id', async(req, res)=>{
+      const item = req.body;
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updatedDoc = {
+        $set:{
+          name:item.name,
+          category:item.category,
+          price:item.price,
+          recipe:item.recipe,
+          image:item.image
+        }
+      }
+      const result = await menuCollection.updateOne(filter, updatedDoc);
+      res.send(result)
+    })
+
 
 
 
