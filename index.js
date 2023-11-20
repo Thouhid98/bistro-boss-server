b3246b2c060c152ac99b0fee2025556232b36d23
Thouhid98 <thouhidhussain98@gmail.com>
2023-11-20 22:48:32 +0600

    User payment history api done

--- index.js
+++ index.js
@@ -232,6 +232,25 @@ async function run() {
       })
     })
 
+    // Save Payment History In the database 
+    const paymentCollection = client.db('bistroDb').collection('payments')
+
+    app.post('/payments', async (req, res) => {
+      const payment = req.body
+      const paymentResult = await paymentCollection.insertOne(payment)
+      console.log('Payment Info', payment);
+
+      // Carefully Delete each item from the cart ordered 
+      const query = {
+        _id: {
+          $in: payment.cartId.map(id => new ObjectId(id))
+        }
+      }
+      const deleteResult = await cartCollection.deleteMany(query)
+      res.send({ paymentResult, deleteResult })
+
+    })
+
 
 
 
